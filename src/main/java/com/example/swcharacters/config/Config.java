43f0bfd10a5b5f4c1cb3fe70ccb4c643b39d1769
Miki <aivi.pol@gmail.com>
2020-10-29 00:19:33 +0100

    Little clean up and pom refactor

--- src/main/java/com/example/swcharacters/config/Config.java
+++ src/main/java/com/example/swcharacters/config/Config.java
@@ -1,8 +1,5 @@
 package com.example.swcharacters.config;
 
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -13,9 +10,7 @@ public class Config {
 
     @Bean
     public RestTemplate restTemplate() {
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
         return new RestTemplate(requestFactory);
     }
 
