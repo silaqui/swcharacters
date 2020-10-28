87136ef92a7fc36c2ca525082d8cd9655a7b102c
Miki <aivi.pol@gmail.com>
2020-10-28 17:19:07 +0100

    Initial commit

---
+++ src/test/java/com/example/swcharacters/StarWarsCharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swcharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsCharactersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
